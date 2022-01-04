cb26305330fc96e64d58eed220b28d6e2284e222
duartecancela <duartecancela@gmail.com>
2022-01-04 16:12:51 +0000

    Add project files.

---
+++ Supermercado/Controllers/Fruit.cs
@@ -0,0 +1,18 @@
+﻿namespace Supermercado.Controllers
+{
+    public class Fruit
+    {
+        public int id;
+        public string name;
+        public double price;
+
+        // Parameterized Constructor
+        // User defined
+        public Fruit(int i, string s, double d)
+        {
+            id = i;
+            name = s;
+            price = d;
+        }
+    }
+}
---
+++ Supermercado/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Supermercado.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        Fruit banana = new(1, "Banana", 2.4);
+        Fruit orange = new(2, "Orange", 1.4);
+        Fruit Apple = new(1, "Apple", 1,8);
+
+        public IActionResult Index()
+        {
+            ViewData["name"] = banana.name;
+            return View();
+           
+        }
+    }
+}
